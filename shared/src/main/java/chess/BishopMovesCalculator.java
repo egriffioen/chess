51abf45a9bf85f44cf0971e8eebcea14293f9772
egriffioen <ellagriffioen@gmail.com>
2025-01-15 23:21:29 -0700

    ChessMoveTests passed

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -1,4 +1,14 @@
 package chess;
 
+import java.util.Collection;
+import java.util.List;
+
 public class BishopMovesCalculator implements PieceMovesCalculator {
+    public BishopMovesCalculator() {
+
+    }
+
+    public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition) {
+        return List.of();
+    }
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,4 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public interface PieceMovesCalculator {
+    public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition);
 }
