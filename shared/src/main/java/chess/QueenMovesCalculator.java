4ee402037504a22e8b0dd29ac972ceb7db51a49d
egriffioen <ellagriffioen@gmail.com>
2025-03-03 18:52:02 -0700

    Cleaned up QueenMovesCalculator

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -10,53 +10,10 @@ public class QueenMovesCalculator implements PieceMovesCalculator {
     }
 
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition) {
-        int up = 1;
-        int down = -1;
-        int left = -1;
-        int right = 1;
-        addMoves(board, myPosition, right, up); //diagonal right up
-        addMoves(board, myPosition, right, down); //diagonal right down
-        addMoves(board, myPosition, left, up); //diagonal left up
-        addMoves(board, myPosition, left, down); //diagonal right down
-        addMoves(board, myPosition, right, 0);//move right
-        addMoves(board, myPosition, 0, up); //move up
-        addMoves(board, myPosition, left, 0); //move left
-        addMoves(board, myPosition, 0, down); //move down
+        RookMovesCalculator horizontalMoves = new RookMovesCalculator();
+        moves.addAll(horizontalMoves.calculateMoves(board, myPosition));
+        BishopMovesCalculator diagonalMoves = new BishopMovesCalculator();
+        moves.addAll(diagonalMoves.calculateMoves(board, myPosition));
         return moves;
-    }
-
-    public void addMoves(ChessBoard board, ChessPosition myPosition, int rowMove, int colMove) {
-        for (int i = 0; i < 8; i++) {
-
-            ChessPosition end = new ChessPosition(myPosition.getRow()+rowMove, myPosition.getColumn()+colMove);
-            if(end.getRow()==9||end.getColumn()==9||end.getRow()==0||end.getColumn()==0) {
-                break;
-            }
-            else if(board.getPiece(end)==null) {
-                ChessMove move = new ChessMove(myPosition, end, null);
-                moves.add(move);
-            }
-            else if (board.getPiece(end).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                ChessMove move = new ChessMove(myPosition, end, null);
-                moves.add(move);
-                break;
-            }
-            else if (board.getPiece(end).getTeamColor() == board.getPiece(myPosition).getTeamColor()) {
-                break;
-            }
-
-            if (rowMove>0) {
-                rowMove++;
-            }
-            else if (rowMove<0) {
-                rowMove--;
-            }
-            if (colMove >0) {
-                colMove++;
-            }
-            else if (colMove<0) {
-                colMove--;
-            }
-        }
     }
 }
