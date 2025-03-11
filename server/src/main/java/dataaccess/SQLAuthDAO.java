a453b33337863ab9173a15b6e3f880c9c302bfb0
egriffioen <ellagriffioen@gmail.com>
2025-03-11 12:31:41 -0600

    AuthDAO table can be created

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -1,10 +1,17 @@
 package dataaccess;
 
 import model.AuthData;
+import model.UserData;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 
 public class SQLAuthDAO implements AuthDAO{
+
+    public SQLAuthDAO() throws DataAccessException, SQLException {
+        configureDatabase();
+    }
+
     @Override
     public void addAuthToken(AuthData authData) {
 
@@ -28,5 +35,52 @@ public class SQLAuthDAO implements AuthDAO{
     @Override
     public HashMap<String, AuthData> getAuthTokens() {
         return null;
+    }
+
+    private void executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param instanceof AuthData p) ps.setString(i + 1, p.toString());
+                    //else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+//                var rs = ps.getGeneratedKeys();
+//                if (rs.next()) {
+//                    return rs.getInt(1);
+//                }
+//
+//                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS auth (
+            `authToken` varchar(256) NOT NULL,
+            `username` varchar(256) NOT NULL,
+            PRIMARY KEY(`authToken`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private void configureDatabase() throws DataAccessException, SQLException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try(var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+
     }
 }
