ae98e8156f72b1ee2ebf6c944a3b4d968c30fea8
egriffioen <ellagriffioen@gmail.com>
2025-03-23 18:21:31 -0600

    Set up Repl class

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,4 +1,44 @@
 package ui;
 
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
 public class Repl {
+    private final PreLoginClient preLoginClient;
+
+    public Repl(String serverUrl) {
+        preLoginClient = new PreLoginClient(serverUrl, this);
+    }
+
+    public void run() {
+        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
+        System.out.print(preLoginClient.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = preLoginClient.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+//    public void notify(Notification notification) {
+//        System.out.println(SET_TEXT_COLOR_RED + notification.message());
+//        printPrompt();
+//    }
+
+    private void printPrompt() {
+        System.out.print("\n" + ERASE_LINE + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
+
 }
