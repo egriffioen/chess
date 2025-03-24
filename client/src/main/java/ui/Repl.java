5c6750356ac56b0457bdfe65501149c32598e330
egriffioen <ellagriffioen@gmail.com>
2025-03-23 20:11:08 -0600

    Repl updated to switch between clients

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -6,13 +6,19 @@ import static ui.EscapeSequences.*;
 
 public class Repl {
     private final PreLoginClient preLoginClient;
+    private final PostLoginClient postLoginClient;
+    private final InGameClient inGameClient;
+    private State state;
 
     public Repl(String serverUrl) {
         preLoginClient = new PreLoginClient(serverUrl);
+        postLoginClient = new PostLoginClient(serverUrl);
+        inGameClient = new InGameClient(serverUrl);
+        state = State.SIGNEDOUT;
     }
 
     public void run() {
-        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
+        System.out.println("Welcome to 240 chess");
         System.out.print(preLoginClient.help());
 
         Scanner scanner = new Scanner(System.in);
@@ -22,8 +28,23 @@ public class Repl {
             String line = scanner.nextLine();
 
             try {
-                result = preLoginClient.eval(line);
-                System.out.print(SET_TEXT_COLOR_BLUE + result);
+                if (state == State.SIGNEDOUT) {
+                    result = preLoginClient.eval(line);
+                    System.out.print(SET_TEXT_COLOR_BLUE + result);
+                    if (result.contains("You signed in as")) {
+                        state = State.SIGNEDIN;
+                    }
+                }
+                else if (state == State.SIGNEDIN) {
+                    result = postLoginClient.eval(line);
+                    System.out.print(SET_TEXT_COLOR_BLUE + result);
+                    if (result.contains("You joined")) {
+                        state = State.INGAME;
+                    }
+                    if (result.contains("You logged out")) {
+                        state = State.SIGNEDOUT;
+                    }
+                }
             } catch (Throwable e) {
                 var msg = e.toString();
                 System.out.print(msg);
